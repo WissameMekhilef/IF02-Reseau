9c3008d1723440846c887d908a6528036c9d43d1
V1nc3n7 <vincent.renard45@gmail.com>
2016-11-28 17:31:39 +0100

    Entete

--- part_1/hexercice/hexercice.py
+++ part_1/hexercice/hexercice.py
@@ -1,4 +1,9 @@
+
 #-----------------------------------------------------------------------------------------
+
+#!/usr/bin/env python
+# coding: utf-8
+
 import random
 from sys import argv
 import math
